08080f247a784d22f4165f6ab31f91421612f8c5
SteveRodkissAtBurnside <rds@burnside.school.nz>
2021-03-09 19:13:28 +1300

    added working aimple flask-login version

--- QuizFlaskLogin/run.py
+++ QuizFlaskLogin/run.py
@@ -1,7 +1,8 @@
 from flask import Flask, g, render_template, redirect, request, url_for, flash
 from flask_login.mixins import UserMixin
+from flask_login.utils import login_required, logout_user
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, current_user
 
 #################################################################################
 #initialization
@@ -29,6 +30,8 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(50))
     def __repr__(self):
         return f"Username: {self.username}"
+    def check_password(self,password):
+        return self.password == password
 
 '''don't forget to create the databse using this model from the console!!!!!!!! Use Python repl
     >>>from run import db
@@ -38,7 +41,7 @@ class User(db.Model, UserMixin):
 #required for the user longin
 @login_manager.user_loader
 def load_user(user_id):
-    return User.get(user_id) # Fetch the user from the database
+    return User.query.get(user_id) # Fetch the user from the database
 
 
 #################################################################################
@@ -47,23 +50,45 @@ def load_user(user_id):
 
 @app.route('/login',methods=['GET','POST'])
 def login():
-    pass
+    if request.method == "POST":
+        #we posted the form- try to login
+        #get the user from the forms username field
+        user = User.query.filter_by(username=request.form.get("username")).first()
+        #chek if we got one and that the password's match- MUST USE HASHED PASSWORDS!!! THIS IS JUST A DEMO!!
+        if user and user.check_password(request.form.get("password")):
+            login_user(user)
+            #now current_user is set to this user- redirect back to home
+            return redirect(url_for("home"))
+        #else flash a message?
+        else:
+            flash("Username and password not recognised")
+    return render_template("login.html")
+
+@app.route('/logout', methods=["GET","POST"])
+@login_required
+def logout():
+    '''log out the user- pretty self-explanatory'''
+    logout_user()
+    return redirect(url_for('home'))
 
 
 @app.route('/')
 def home():
+    ''' Display all the questions- users click on  question to display it on a new page '''
     questions = Question.query.all()
     return render_template('home.html', questions=questions)
 
 @app.route('/admin')
+@login_required
 def admin():
     '''the admin route to add and delete questions'''
     questions = Question.query.all()
     return render_template("admin.html", questions=questions)
 
 @app.route('/delete_question', methods=['GET','POST'])
+@login_required
 def delete_question():
-    #get the value og the hidden input named "question_id" in the html template
+    #get the value og the hidden input named "question_id" in the html template if the form exists from the post
     if request.form:
         question_id = request.form.get("question_id")
         #and do sql stuff to remove it! find the question we want to dlete from it's id
@@ -73,6 +98,7 @@ def delete_question():
     return redirect(url_for("admin"))
 
 @app.route('/add_question', methods=['GET','POST'])
+@login_required
 def add_question():
     '''this will add a posted question to the database and redirect to the admin route again'''
     if request.form:
@@ -92,6 +118,7 @@ def ask_question(id):
     if request.method == 'POST':
         #we posted, check answer
         user_answer = request.form.get("answer")
+        #get the answer from the question entry in the database
         actual_answer = question_to_ask.answer
         if user_answer.lower() == actual_answer.lower():
             flash("You are correct!")
